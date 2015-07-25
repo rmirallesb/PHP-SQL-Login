41d7d743a1b098ca6ee35eb851e7dc04bee2b6fc
rmirallesb <raulmb7zgz@gmail.com>
2015-07-25 16:16:44 +0200

    Start session

--- main.php
+++ main.php
@@ -4,15 +4,15 @@
   </head>
   <body>
     <?php
+    session_start();
     if(isset($_SESSION['userweb']))
     {
     ?>
-        <h1>Bienvenido</h1>
+      <h1>Bienvenido</h1>
     <?php
-    }else
-    {
-     echo "Debes de loguearte para ver el contenido del sitio.";
-     echo '<meta http-equiv="Refresh" content="5;url=index.php">';
+    }else{
+      echo "Debes de loguearte para ver el contenido del sitio.";
+      echo '<meta http-equiv="Refresh" content="5;url=index.php">';
     }
     ?>
 	</body>
